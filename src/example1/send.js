27b1f65739da801f8440ba582d55da030ba8e55d
Racchel <racchelvelasco@gmail.com>
2021-12-16 14:00:00 -0300

    adjustment message

--- src/example1/send.js
+++ src/example1/send.js
@@ -12,42 +12,42 @@ amqp.connect("amqp://localhost", function(error0, conection) {
 
         var queue = "beneficiary";
         var msgJSON = {
-            nome: "String",
-            email: "String",
-            sexo: "String",
-            tipoBeneficiario: "F",
-            cpf: "00000000000",
-            endereco: "String",
-            bairro: "String",
-            cidade: "String",
-            uf: "String",
-            pais: "String",
-            ddd: "11",
-            telefone: "999999999",
-            ramal: "String",
-            codigoBanco: "String",
-            agencia: "String",
-            digitoAgencia: "String",
-            contaCorrente: "String",
-            digitoContaCorrente: "String",
-            tipoConta: "String",
-            codigoMatricula: "String",
-            codigoINSS: "String",                     // - opcional
-            tipoINSS: "String",                       // - opcional
-            dataNascimento: "1999-12-31",
-            paisNascimento: "String",                 // - opcional
-            estadoNascimento: "String",               // - opcional
-            cidadeNascimento: "String",               // - opcional
-            dataAposentadoria: "String",
-            cnpj: "String",                           // - AVP apenas
-            nomeInstituidora: "String",               // - AVP apenas
-            codigoConvenio: "String",                 // - AVP apenas
-            codigoFilial: "String",                   // - AVP apenas
-            regimeTributario: "String",
-            numeroProposta: "String",
-            codigoEstruturaVendas: "String",
-            valorReserva: 0000000,
-            codproduto: "0001"
+            "nome": "String",
+            "email": "String",
+            "sexo": "String",
+            "tipoBeneficiario": "F",
+            "cpf": "00000000000",
+            "endereco": "String",
+            "bairro": "String",
+            "cidade": "String",
+            "uf": "String",
+            "pais": "String",
+            "ddd": "11",
+            "telefone": "999999999",
+            "ramal": "String",
+            "codigoBanco": "String",
+            "agencia": "String",
+            "digitoAgencia": "String",
+            "contaCorrente": "String",
+            "digitoContaCorrente": "String",
+            "tipoConta": "String",
+            "codigoMatricula": "String",
+            "codigoINSS": "String",                     // - opcional
+            "tipoINSS": "String",                       // - opcional
+            "dataNascimento": "1999-12-31",
+            "paisNascimento": "String",                 // - opcional
+            "estadoNascimento": "String",               // - opcional
+            "cidadeNascimento": "String",               // - opcional
+            "dataAposentadoria": "String",
+            "cnpj": "String",                           // - AVP apenas
+            "nomeInstituidora": "String",               // - AVP apenas
+            "codigoConvenio": "String",                 // - AVP apenas
+            "codigoFilial": "String",                   // - AVP apenas
+            "regimeTributario": "String",
+            "numeroProposta": "String",
+            "codigoEstruturaVendas": "String",
+            "valorReserva": 0000000,
+            "codproduto": "0001"
         };
 
         var msgString = JSON.stringify(msgJSON);
--- src/example3/client.js
+++ src/example3/client.js
@@ -17,42 +17,42 @@ amqp.connect("amqp://localhost")                            // connecting with r
         });
 
         var msgJSON = {
-            nome: "String",
-            email: "String",
-            sexo: "String",
-            tipoBeneficiario: "F",
-            cpf: "00000000000",
-            endereco: "String",
-            bairro: "String",
-            cidade: "String",
-            uf: "String",
-            pais: "String",
-            ddd: "11",
-            telefone: "999999999",
-            ramal: "String",
-            codigoBanco: "String",
-            agencia: "String",
-            digitoAgencia: "String",
-            contaCorrente: "String",
-            digitoContaCorrente: "String",
-            tipoConta: "String",
-            codigoMatricula: "String",
-            codigoINSS: "String",                     // - opcional
-            tipoINSS: "String",                       // - opcional
-            dataNascimento: "1999-12-31",
-            paisNascimento: "String",                 // - opcional
-            estadoNascimento: "String",               // - opcional
-            cidadeNascimento: "String",               // - opcional
-            dataAposentadoria: "String",
-            cnpj: "String",                           // - AVP apenas
-            nomeInstituidora: "String",               // - AVP apenas
-            codigoConvenio: "String",                 // - AVP apenas
-            codigoFilial: "String",                   // - AVP apenas
-            regimeTributario: "String",
-            numeroProposta: "String",
-            codigoEstruturaVendas: "String",
-            valorReserva: 0000000,
-            codproduto: "0001"
+            "nome": "String",
+            "email": "String",
+            "sexo": "String",
+            "tipoBeneficiario": "F",
+            "cpf": "00000000000",
+            "endereco": "String",
+            "bairro": "String",
+            "cidade": "String",
+            "uf": "String",
+            "pais": "String",
+            "ddd": "11",
+            "telefone": "999999999",
+            "ramal": "String",
+            "codigoBanco": "String",
+            "agencia": "String",
+            "digitoAgencia": "String",
+            "contaCorrente": "String",
+            "digitoContaCorrente": "String",
+            "tipoConta": "String",
+            "codigoMatricula": "String",
+            "codigoINSS": "String",                     // - opcional
+            "tipoINSS": "String",                       // - opcional
+            "dataNascimento": "1999-12-31",
+            "paisNascimento": "String",                 // - opcional
+            "estadoNascimento": "String",               // - opcional
+            "cidadeNascimento": "String",               // - opcional
+            "dataAposentadoria": "String",
+            "cnpj": "String",                           // - AVP apenas
+            "nomeInstituidora": "String",               // - AVP apenas
+            "codigoConvenio": "String",                 // - AVP apenas
+            "codigoFilial": "String",                   // - AVP apenas
+            "regimeTributario": "String",
+            "numeroProposta": "String",
+            "codigoEstruturaVendas": "String",
+            "valorReserva": 0000000,
+            "codproduto": "0001"
         };
 
         var msgString = JSON.stringify(msgJSON);            // transforming messages to string
